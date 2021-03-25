c10ea540740d43b1e96230cc737531a94101f1f5
golden-state0fmind <areyes2009@yahoo.com>
2021-03-24 23:48:03 -0700

    adds home button

--- pages/excercise.js
+++ pages/excercise.js
@@ -1,7 +1,13 @@
 import styles from '../styles/Home.module.css'
+import Button from '@material-ui/core/Button'
 
 function Excercise(){
-    return (<div className={ styles.card } >Welcome to the workout page</div>)
+    return (
+    <div className={ styles.card } >
+        <Button variant='contained' color='primary' href='/' > Home </Button> <br/>
+        Welcome to the workout page
+    </div>
+        )
 }
 
 export default Excercise
