2d69b9b4d89c1f6abc3afb7e0cac97044f6e327f
golden-state0fmind <areyes2009@yahoo.com>
2021-03-24 23:48:49 -0700

    adds home button

--- pages/yoga.js
+++ pages/yoga.js
@@ -1,8 +1,10 @@
 import styles from '../styles/Home.module.css'
+import Button from '@material-ui/core/Button'
 
 function About(){
     return(
     <div className={styles.card} > 
+        <Button variant='contained' color='primary' href='/' > Home </Button> <br/>
         Welcome to the yoga section
     </div>
     ) 
