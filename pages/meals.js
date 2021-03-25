614828db157552cd70ea8261928d2f1d2767000e
golden-state0fmind <areyes2009@yahoo.com>
2021-03-24 22:48:37 -0700

    adds meals page

--- pages/meals.js
+++ pages/meals.js
@@ -1,5 +1,17 @@
+import styles from '../styles/Home.module.css'
+import MenuCard from '../components/MenuCard'
+
 function MealRecipes(){
-    return <div>Welcome to the meal recipes for vegans and vegetarians</div>
+    return (
+    <>
+        <div className={styles.card}>
+            Welcome to the meal recipes for vegans and vegetarians
+        </div>
+        <div className={styles.container}>
+            <MenuCard/>
+        </div>
+    </>
+    )
 }
 
 export default MealRecipes
