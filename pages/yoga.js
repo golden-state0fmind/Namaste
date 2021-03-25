c3105db224103e9c1b333dc3e00848a863406563
golden-state0fmind <areyes2009@yahoo.com>
2021-03-24 23:22:04 -0700

    adds styling to the header of the page

--- pages/yoga.js
+++ pages/yoga.js
@@ -1,5 +1,11 @@
+import styles from '../styles/Home.module.css'
+
 function About(){
-    return <div> Welcome to the yoga section</div>
+    return(
+    <div className={styles.card} > 
+        Welcome to the yoga section
+    </div>
+    ) 
 }
 
 export default About
