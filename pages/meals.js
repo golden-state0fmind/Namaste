92dc45432199c8461e7f9755f93c985f6a26c883
golden-state0fmind <areyes2009@yahoo.com>
2021-03-24 23:43:32 -0700

    adds home button to meals page

--- pages/meals.js
+++ pages/meals.js
@@ -1,10 +1,12 @@
 import styles from '../styles/Home.module.css'
 import MenuCard from '../components/MenuCard'
+import Button from '@material-ui/core/Button'
 
 function MealRecipes(){
     return (
     <>
         <div className={styles.card}>
+            <Button variant='contained' color='primary' href='/' > Home </Button> <br/>
             Welcome to the meal recipes for vegans and vegetarians
         </div>
         <div className={styles.container}>
