0cbaaee0db056d5589b1ed44718fd86bb836d77b
golden-state0fmind <areyes2009@yahoo.com>
2021-03-24 23:48:25 -0700

    add home button

--- pages/motivation.js
+++ pages/motivation.js
@@ -1,7 +1,13 @@
 import styles from '../styles/Home.module.css'
+import Button from '@material-ui/core/Button'
 
 function Motivation(){
-    return (<div className={styles.card} >Welcome to the motivaion page</div>)
+    return (
+    <div className={styles.card} >
+        <Button variant='contained' color='primary' href='/' > Home </Button> <br/>
+        Welcome to the motivaion page
+    </div>
+    )
 }
 
 export default Motivation
