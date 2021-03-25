f4f3cec23901433efd4e27395891da6633e8bc4f
golden-state0fmind <areyes2009@yahoo.com>
2021-03-24 23:21:16 -0700

    adds styling to header on the page

--- pages/motivation.js
+++ pages/motivation.js
@@ -1,5 +1,7 @@
+import styles from '../styles/Home.module.css'
+
 function Motivation(){
-    return <div>Welcome to the motivaion page</div>
+    return (<div className={styles.card} >Welcome to the motivaion page</div>)
 }
 
 export default Motivation
