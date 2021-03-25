5bbb9c4d7131553f09cfc82f9a9e1fff961ed56a
golden-state0fmind <areyes2009@yahoo.com>
2021-03-24 23:20:44 -0700

    starts affirmation mockAPI

---
+++ components/Affirmations.js
@@ -0,0 +1,5 @@
+const affirmations = [ 'You can do this!', 'Shine bright today!', 'Believe in the magic within', 'You are beautiful', 'You are the light in the dark', 'You are an abundance of love and light', 'You will thrive and prosper just be patient with yourself', 'Cheers to your beautiful life', 'Free yourself and dance with life' ]
+
+for (let i = 0; i < affirmations.length; i++){
+    return affirmations[i]
+}
