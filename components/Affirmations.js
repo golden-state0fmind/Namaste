ddd4e0419fed96f949e63fb53e639d6a0c7112c0
golden-state0fmind <areyes2009@yahoo.com>
2021-03-28 01:15:50 -0700

    displays random affirmation

--- components/Affirmations.js
+++ components/Affirmations.js
@@ -1,5 +1,7 @@
-const affirmations = [ 'You can do this!', 'Shine bright today!', 'Believe in the magic within', 'You are beautiful', 'You are the light in the dark', 'You are an abundance of love and light', 'You will thrive and prosper just be patient with yourself', 'Cheers to your beautiful life', 'Free yourself and dance with life' ]
 
-for (let i = 0; i < affirmations.length; i++){
-    return affirmations[i]
+export default function Affirmations() {
+    const affirmations = [ 'You can do this!', 'Shine bright today!', 'Believe in the magic within', 'You are beautiful', 'You are the light in the dark', 'You are an abundance of love and light', 'You will thrive and prosper just be patient with yourself', 'Cheers to your beautiful life', 'Free yourself and dance with life', 'To persevere is favorbale' ]
+    const randomAffirmation = Math.floor(Math.random()*affirmations.length) 
+    console.log(affirmations[randomAffirmation])
+    return affirmations[randomAffirmation]
 }
