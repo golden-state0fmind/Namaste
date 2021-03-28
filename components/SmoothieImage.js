84040a1f5bb4015e3e2608b47c2ac4921c08eff6
golden-state0fmind <areyes2009@yahoo.com>
2021-03-27 23:43:28 -0700

    imports image component from next and attempts image handling in next.js

---
+++ components/SmoothieImage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+
+const SmoothieImage = () =>{
+    return (
+        <Image
+        src="/assets/berrySmoothie.png"
+        alt=""
+        height={500}
+        width={500}
+        />
+    )
+}
+
+export default SmoothieImage
