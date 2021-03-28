66115a36214f44f35ab4022be841bdebf069b3c6
golden-state0fmind <areyes2009@yahoo.com>
2021-03-28 01:16:18 -0700

    places random affirmation in a card

--- pages/motivation.js
+++ pages/motivation.js
@@ -1,12 +1,18 @@
 import styles from '../styles/Home.module.css'
 import Button from '@material-ui/core/Button'
+import Affirmations from '../components/Affirmations'
 
 function Motivation(){
     return (
-    <div className={styles.card} >
-        <Button variant='contained' color='primary' href='/' > Home </Button> <br/>
-        Welcome to the motivaion page
-    </div>
+    <>
+        <div className={styles.card} >
+            <Button variant='contained' color='primary' href='/' > Home </Button> <br/>
+            Welcome to the motivaion page
+        </div>
+        <div className={styles.card}>
+        <Affirmations/> ☮️
+        </div>
+    </>
     )
 }
 
