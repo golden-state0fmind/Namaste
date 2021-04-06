c885ae11fcae48fc0abea64ca9946ce5c77a5130
golden-state0fmind <areyes2009@yahoo.com>
2021-04-05 22:57:54 -0700

    imports new style sheet for the meals page

--- pages/meals.js
+++ pages/meals.js
@@ -1,18 +1,18 @@
-import styles from '../styles/Home.module.css'
+import style from '../styles/Meals.module.css'
 import MenuCard from '../components/MenuCard'
 import Button from '@material-ui/core/Button'
 
 function MealRecipes(){
     return (
-    <>
-        <div className={styles.card}>
+    <div className={style.body}>
+        <div className={style.card}>
             <Button variant='contained' color='primary' href='/' > Home </Button> <br/>
             Welcome to the meal recipes for vegans and vegetarians
         </div>
-        <div className={styles.container}>
+        <div className={style.container}>
             <MenuCard/>
         </div>
-    </>
+    </div>
     )
 }
 
