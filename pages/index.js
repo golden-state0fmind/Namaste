dcf21507dc82108142387b91c940c6f0f71f0b40
golden-state0fmind <areyes2009@yahoo.com>
2021-04-07 23:47:26 -0700

    pivots yoga section to comment section

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,23 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Namaste</title>
+        <title>Namste</title>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to the <a href="https://www.antonioreyes.dev"> Namaste Lifestyle!</a>
+          Welcome and start living a balanced <a href="https://www.antonioreyes.dev">Lifestyle!</a>
         </h1>
 
         <div className={styles.grid}>
-          <a href="/yoga" className={styles.card}>
-            <h3>Yoga &rarr;</h3>
-            <p>Here you can seek guidence into different yoga poses to stay flexible with lifes challenges.</p>
+          <a
+            href="/motivation"
+            className={styles.card}
+          >
+            <h3>Motivation &rarr;</h3>
+            <p>Not feeling pumped today? That's okay feel free to drop a blurb for some uplifting responses.</p>
           </a>
-
+          
           <a href="/excercise" className={styles.card}>
             <h3>Excercise &rarr;</h3>
             <p>This will be a reference point to look into different workouts to stay fit and happy.</p>
@@ -32,12 +35,9 @@ export default function Home() {
             <p>Flavor jam packed vegan and vegetarian recipes will be shared weekly as a tasty treat.</p>
           </a>
 
-          <a
-            href="/motivation"
-            className={styles.card}
-          >
-            <h3>Motivation &rarr;</h3>
-            <p>Not feeling pumped today? That's okay feel free to drop a blurb for some uplifting responses.</p>
+          <a href="/comments" className={styles.card}>
+            <h3>Share here &rarr;</h3>
+            <p>Leave comments here about some of your favorite ways of keeping balance.</p>
           </a>
         </div>
       </main>
