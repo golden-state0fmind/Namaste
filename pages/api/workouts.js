5b587189ec3866caa944b5404eba92998c1cd268
golden-state0fmind <areyes2009@yahoo.com>
2021-11-30 22:20:21 -0700

    moves all the files into src

--- pages/api/workouts.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import backExcercise from "./backExcercise.json"
-
-export default function workouts (req, res){
-  res.statusCode = 200
-  res.json(backExcercise)
-}
