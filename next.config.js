1109520ff88004830eb6125351149d70f32cafdf
golden-state0fmind <areyes2009@yahoo.com>
2021-11-30 22:43:48 -0700

    changes the next.config file to compile deployment build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 module.exports = {
     webpack5: false,
     webpack(config) {
@@ -12,4 +13,12 @@ module.exports = {
         });
         return config;
     },
+    env: {
+        apiKey: process.env.api_Key,
+        authDomain: process.env.auth_Domain,
+        projectId: process.env.project_Id,
+        storageBucket: process.env.storage_Bucket,
+        messagingSenderId: process.env.messaging_SenderId,
+        appId: process.env.app_Id
+    }
 };
