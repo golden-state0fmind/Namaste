4e250a3b9909ace6661f00cee3993717001f4f89
golden-state0fmind <areyes2009@yahoo.com>
2022-07-19 00:14:37 -0700

    cleans out unused values

---
+++ src/components/MainLayout.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Testimonycard from '../components/utilities/TestimonyCard';
+const axios = require('axios');
+
+
+const Mainlayout = () => {
+    //axios.post('/api/posts', { title: "Foo Bar", postBody: "foo-bar", body: "lorem ipsum" })
+    useEffect(() => {
+        //effect
+        return () => {
+            //cleanup
+        };
+    }, []);
+    return (
+        <>
+            <main className={styles.main}>
+                <Head>
+                    <title>Namaste</title>
+                </Head>
+                <div className={styles.grid}>
+                    <h1 className={styles.title}>Welcome and explore!</h1>
+                    <h3 className={styles.welcome_message}>Here we advocate healthier choices for people seeking a change but don't know where to start</h3>
+                </div>
+            </main>
+        </>
+    );
+}
+
+export default Mainlayout;
