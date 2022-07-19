7c33ef214aff38d2349707b7896774a52eb844a6
golden-state0fmind <areyes2009@yahoo.com>
2022-07-19 00:08:30 -0700

    updates values

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,33 @@
 // Required for side-effects
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+import 'firebase/compat/firestore';
+import {
+    getAuth,
+    onAuthStateChanged,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "firebase/auth";
+
 // Initialize Cloud Firestore through Firebase
 const firebaseConfig = {
-    apiKey: process.env.api_Key,
-    authDomain: process.env.auth_Domain,
-    projectId: process.env.project_Id,
-    storageBucket: process.env.storage_Bucket,
-    messagingSenderId: process.env.messaging_SenderId,
-    appId: process.env.app_Id
+    apiKey: process.env.REACT_APP_MY_KEY,
+    authDomain: process.env.REACT_APP_MY_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_MY_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_MY_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MY_MESSAGING_SENDERID,
+    appId: process.env.REACT_APP_MY_APP_ID
 };
 // Initialize Firebase
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-};
+const app = initializeApp(firebaseConfig);
 //export
-const firestore = firebase.firestore();
+//const firestore = firebase.firestore(app);
+const auth = getAuth()
 
-export { firebase, firestore };
+export {
+    firebase,
+    auth,
+    onAuthStateChanged,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+};
