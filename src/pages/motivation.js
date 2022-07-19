6dc9f09dbc79d465b46e4059676f70bcb6822d86
golden-state0fmind <areyes2009@yahoo.com>
2022-07-19 00:11:23 -0700

    changes layout

--- src/pages/motivation.js
+++ src/pages/motivation.js
@@ -1,20 +1,16 @@
-import styles from '../styles/Home.module.css'
-import Button from '@material-ui/core/Button'
-import Affirmations from '../components/Affirmations'
-import HomeIcon from '@material-ui/icons/Home';
+import styles from '../styles/Home.module.css';
+import Affirmations from '../components/Affirmations';
 
-function Motivation(){
+export default function Motivation() {
     return (
-    <>
-        <div className={styles.card} >
-            <Button variant='contained' color='primary' href='/' > Home <HomeIcon/></Button> <br/>
+        <>
+            <div className={styles.card} >
+                <a variant='contained' color='primary' href='/' > Home </a> <br />
             Welcome to the motivaion page
         </div>
-        <div className={styles.card}>
-            <Affirmations/> ☮️
+            <div className={styles.card}>
+                <Affirmations /> ☮️
         </div>
-    </>
+        </>
     )
 }
-
-export default Motivation
