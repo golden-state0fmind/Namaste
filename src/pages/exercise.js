2843dc90c4f306aa2b41696957be0ff92b7add93
golden-state0fmind <areyes2009@yahoo.com>
2022-07-19 00:12:30 -0700

    changes layout

--- src/pages/exercise.js
+++ src/pages/exercise.js
@@ -1,24 +1,19 @@
 import React from 'react';
 //import {workouts} from './api/workouts'
 import styles from '../styles/Exercise.module.css';
-import Button from '@material-ui/core/Button';
-import HomeIcon from '@material-ui/icons/Home';
 
-function Exercise() {
-    
+export default function Exercise() {
+
     return (
         <>
             <div className={styles.body}>
                 <div className={styles.card} >
-                    <Button variant='contained' color='primary' href='/' > Home <HomeIcon /> </Button> <br />
-                Welcome to the workout page
+                    Welcome to the workout page
             </div>
                 <h3 className={styles.card} >
                     Under Construction this page will be up soon!
-            </h3>
+                </h3>
             </div>
         </>
     );
 };
-
-export default Exercise;
