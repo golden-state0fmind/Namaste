b5862ead140c1cfe36d97594c6137e52a5b876c3
golden-state0fmind <areyes2009@yahoo.com>
2022-07-19 00:28:28 -0700

    comments out posts call causing build to fail

--- src/pages/api/posts/index.js
+++ src/pages/api/posts/index.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { firestore } from '../../../firebase';
 
-export default async (req, res) => {
-    try {
-        const { postBody } = req.body;
-        const posts = await firestore.collection("Posts").get();
-        const postsData = await posts.docs.map(entry => entry.data());
-        console.log(postsData, "in the posts api call")
-        res.status(200).json({ id });
-        if (postsData.some(entry => { entry.postBody === postBody })) {
-            res.status(400).end();
-        } else {
-            const { id } = await firestore.collection("Posts").add({
-                ...req.body,
-                created: new Date().toISOString(),
-            });
-        };
-    }
-    catch (e) {
-        res.status(400).end()
-    };
-};
-
+// export default async (req, res) => {
+//     try {
+//         const { postBody } = req.body;
+//         const posts = await firestore.collection("Posts").get();
+//         const postsData = await posts.docs.map(entry => entry.data());
+//         console.log(postsData, "in the posts api call")
+//         res.status(200).json({ id });
+//         if (postsData.some(entry => { entry.postBody === postBody })) {
+//             res.status(400).end();
+//         } else {
+//             const { id } = await firestore.collection("Posts").add({
+//                 ...req.body,
+//                 created: new Date().toISOString(),
+//             });
+//         };
+//     }
+//     catch (e) {
+//         res.status(400).end()
+//     };
+// };
