6d126d0ef60aa03b9cfd74f1f6a78372107c6bd5
golden-state0fmind <areyes2009@yahoo.com>
2022-07-19 00:09:01 -0700

    adds protective routes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,42 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/Home.module.css';
+import { useRouter } from 'next/router';
+import Navbar from '../components/utilities/NavBar';
+import Footer from '../components/utilities/Footer';
+import ProtectedRoute from '../components/ProtectedRoute';
+import { AuthContextProvider } from '../context/AuthContext';
+import { ThemeContextProvider } from '../context/ThemeContext';
+
+const noAuthRequired = ['/', '/signin', '/signup'];
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles)
     }
   }, []);
-  return <Component {...pageProps} />
+  return (
+    <AuthContextProvider>
+      <ThemeContextProvider>
+        <Navbar />
+        <main className={styles.main}>
+          {noAuthRequired.includes(router.pathname) ? (
+            <Component {...pageProps} />
+          ) : (
+              <ProtectedRoute>
+                <Component {...pageProps} />
+              </ProtectedRoute>
+            )}
+        </main>
+        <Footer />
+      </ThemeContextProvider>
+    </AuthContextProvider>
+  );
 };
 
 export default MyApp;
