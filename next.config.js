a47be2dab2ac5f7a51a11863d0222317ef25aa01
golden-state0fmind <areyes2009@yahoo.com>
2022-07-22 17:45:18 -0700

    starts working meals api

--- next.config.js
+++ next.config.js
@@ -19,6 +19,8 @@ module.exports = {
         REACT_APP_MY_PROJECT_ID: process.env.REACT_APP_MY_PROJECT_ID,
         REACT_APP_MY_STORAGE_BUCKET: process.env.REACT_APP_MY_STORAGE_BUCKET,
         REACT_APP_MY_MESSAGING_SENDERID: process.env.REACT_APP_MY_MESSAGING_SENDERID,
-        REACT_APP_MY_APP_ID: process.env.REACT_APP_MY_APP_ID
+        REACT_APP_MY_APP_ID: process.env.REACT_APP_MY_APP_ID,
+        REACT_APP_FOOD_KEY_ID: process.env.REACT_APP_FOOD_KEY_ID,
+        REACT_APP_FOOD_KEY: process.env.REACT_APP_FOOD_KEY
     }
 };
--- src/pages/meals.js
+++ src/pages/meals.js
@@ -1,14 +1,57 @@
+import React, { useState, useEffect } from 'react';
+const axios = require('axios');
 import style from '../styles/Meals.module.css';
 
 export default function MealRecipes() {
+    const REACT_APP_FOOD_KEY_ID = process.env.REACT_APP_FOOD_KEY_ID;
+    const REACT_APP_FOOD_KEY = process.env.REACT_APP_FOOD_KEY;
+    const urlAPI = `https://api.edamam.com/api/recipes/v2?type=public&q=tofu&app_id=${REACT_APP_FOOD_KEY_ID}&app_key=%20${REACT_APP_FOOD_KEY}%09&ingr=5-10&diet=high-protein&health=vegan&cuisineType=American&mealType=Dinner&imageSize=REGULAR`;
+    const [recipe, setRecipe] = useState([]);
+    const [show, setShow] = useState(false);
+    const getRecipes = async() => {
+        try {
+            const response = await axios(urlAPI)
+            console.log(response)
+            setRecipe(response.data)
+            }
+        catch (error) {
+            console.error(error)
+        };
+    }
+    const getRecipeData = () => {
+        console.log(recipe.hits[0].recipe.label, recipe.hits[0].recipe.image,'GETTING RECIPE')
+    }
+    useEffect(() => {
+        //effect
+        getRecipes()
+        console.log(recipe, 'TESTING THE STATE OF THE RETURNED RESPONSE')
+        return () => {
+            //cleanup
+        };
+    }, []);
+
+
     return (
         <div className={style.body}>
+            <button onClick={getRecipes} >Get More Recipes</button>
+            <button onClick={getRecipeData} >Find More Recipes</button>
+            <button onClick={() => setShow(!show)} >Find More Recipes</button>
             <div className={style.card}>
                 Welcome to the meal recipes for vegans and vegetarians
             </div>
+            {/* will need a label, cautions, dietLabels, images, ingredientLines:[], URL, credit, */}
             <div className={style.card}>
-                <h3>CARD &rarr;</h3>
-                <p>Friendly reminders to be better than yesterday.</p>
+                {!show
+                    ? (
+                        null
+                    )
+                    : (
+                        recipe.hits.map((element, i) => (
+                            <div key={i} style={{width:'100%', height:'100%'}}>
+                                <img src={element.recipe.image} />
+                            </div>
+                        ))
+                    )}
             </div>
         </div>
     );
