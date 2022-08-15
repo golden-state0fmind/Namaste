71f605d7a51e65eeb621e44936ad2d5bdee8fcec
golden-state0fmind <areyes2009@yahoo.com>
2022-08-14 21:32:29 -0700

    changes the comment page styles

--- src/pages/comments.js
+++ src/pages/comments.js
@@ -4,11 +4,10 @@ import styles from '../styles/Home.module.css';
 export default function About() {
     return (
         <>
-            <div className={styles.card}>
-                <div className={styles.card}>
-                    <h3>CARD &rarr;</h3>
-                    <p>Flavor jam packed vegan and vegetarian recipes will be shared weekly as a tasty treat.</p>
-                </div>
+            <div className='container-fluid d-flex flex-column'>
+                <h4 className='display-4 text-center mb-3'>
+                    ⚙️ Comment section will come soon 🚧
+            </h4>
             </div>
         </>
     );
